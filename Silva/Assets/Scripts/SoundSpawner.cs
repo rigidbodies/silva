975133c2ced8607a43ee86b208b8970f989f9419
KatjaFrey <85015463+KatjaFrey@users.noreply.github.com>
2021-06-25 17:50:53 +0200

    Added sounds for extra points + exchanged checkpoint sprites

---
+++ Silva/Assets/Scripts/SoundSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSpawner : MonoBehaviour
+{
+    [SerializeField] private int scoreValue = 10;
+    [SerializeField] private float spawnTime = 8.0f;
+
+    private float timeCounter = 0.0f;
+    private bool pointsCollected = false;
+
+    private AudioSource extraPointSound;
+    private CharacterController2D character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraPointSound = GetComponent<AudioSource>();
+        character = FindObjectOfType<CharacterController2D>();
+
+        // Make sure spawnTime isn't shorter than audio clip
+        if (spawnTime < 3.5f)
+        {
+            spawnTime = 3.5f;
+        }
+
+        //Randomise spawnTime
+        spawnTime += Random.Range(0.0f,3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime;
+
+        if(timeCounter >= spawnTime)
+        {
+            timeCounter = 0;
+            if (!extraPointSound.isPlaying)
+            {
+                pointsCollected = false;
+                extraPointSound.Play();
+            }
+        }
+
+        bool enterPressed = Input.GetKeyDown(KeyCode.Return);
+
+        if(extraPointSound.isPlaying && enterPressed && !pointsCollected)
+        {
+            pointsCollected = true;
+            character.IncreaseScore(scoreValue);
+        }
+    }
+}
