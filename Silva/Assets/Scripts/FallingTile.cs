2eff1cdf705d1af11775054bcbbbe52ed7946e68
KatjaFrey <85015463+KatjaFrey@users.noreply.github.com>
2021-07-13 18:02:33 +0200

    Added falling tiles

---
+++ Silva/Assets/Scripts/FallingTile.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+    [SerializeField] private float fallingSpeed = 4.0f;
+    [SerializeField] private float recoveringSpeed = 2.0f;
+    private bool falling = false;
+    private bool recovering = false;
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (falling)
+        {
+            transform.position += new Vector3(0, -fallingSpeed, 0) * Time.deltaTime;
+        }
+
+        if (recovering && this.transform.position != originalPosition)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, originalPosition, Time.deltaTime * recoveringSpeed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            recovering = false;
+            falling = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            falling = false;
+            recovering = true;
+        }
+    }
+}
